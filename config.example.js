49164bcdf55c3a7383d128926ad3a6190809068d
TreZc0_ <TreZc0@gmx.de>
2019-05-04 13:46:22 +0200

    update example config

--- config.example.js
+++ config.example.js
@@ -2,9 +2,12 @@ module.exports = {
     "discord-response-channel-id": '123456789123456789',
     "discord-notifications-channel-id": '123456789123456789',
     "target-game-id": '5635',
-    "bot-currently-playing": 'The Minish Cap',
-  
+    "bot-currently-playing": 'TGame Name',
+    "bot-user-name": 'SuperBot',
+    "bot-avatar-url": 'https://cdn.discordapp.com/avatars/574179086591590420/42331bf5233c7c7203a8e7d794199191.png',
+     
     "twitch-client-id": "twitch dev token",
-    "discord-token": "discord bot token"
+    "discord-token": "discord bot token",
+    "colors": ["teal", "red", "orange", "yellow", "green", "blue", "purple", "none"]
   };
   
