37a4d4993090063c379cd7e57ffe61716df68b58
Chris W <trezc0@gmx.de>
2019-05-08 20:51:21 +0200

    Updated Config Example

--- config.example.js
+++ config.example.js
@@ -1,13 +1,12 @@
 module.exports = {
-    "discord-response-channel-id": '123456789123456789',
-    "discord-notifications-channel-id": '123456789123456789',
-    "target-game-id": '5635',
-    "bot-currently-playing": 'TGame Name',
-    "bot-user-name": 'SuperBot',
-    "bot-avatar-url": 'https://cdn.discordapp.com/avatars/574179086591590420/42331bf5233c7c7203a8e7d794199191.png',
-     
-    "twitch-client-id": "twitch dev token",
-    "discord-token": "discord bot token",
-    "colors": ["teal", "red", "orange", "yellow", "green", "blue", "purple", "none"]
-  };
-  
+  "discord-response-channel-id": '123456789123456789',
+  "discord-notifications-channel-id": '123456789123456789',
+  "discord-colors-channel-id": '123456789123456789',
+  "target-game-id": '110758',
+  "bot-user-name": 'Bot',
+  "bot-avatar-url": 'https://cdn.discordapp.com/avatars/574179086591590420/42331bf5233c7c7203a8e7d794199191.png',
+  "bot-currently-playing": 'Breath of the Wild',
+  "twitch-client-id": "twitch client token",
+  "discord-token": "discord bot token",
+  "colors": ["red", "orange", "yellow", "green", "blue", "purple", "none"]
+};
